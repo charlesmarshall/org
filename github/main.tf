ad74e742f1e322283d6f881a08f2fdfcda5226b1
Charles Marshall <cm56marshall@gmail.com>
2021-05-22 23:29:59 +0100

    move to a sub folder for cleaner org

--- github/main.tf
+++ github/main.tf
@@ -1,16 +1,9 @@
 terraform {
   required_version = ">= 0.15"
-  backend "local" {
-    path = "./terraform.tfstate"
-  }
   required_providers {
     github = {
       version = "~> 4.9"
       source  = "integrations/github"
     }
   }
-}
-
-module "github" {
-  source  = "./github"
 }
