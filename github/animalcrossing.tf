6eb76f203dfd1a141a6f104a0ab7444f5746ac18
Charles Marshall <cm56marshall@gmail.com>
2021-05-23 15:11:57 +0100

    add new repos

--- github/animalcrossing.tf
+++ github/animalcrossing.tf
@@ -4,7 +4,7 @@ resource "github_repository" "api-conversion" {
   name                   = "api-conversion"
   description            = "Download and convert data from acnhapi.com"
   visibility             = "public"
-  archived               = false
+  archived               = true
   auto_init              = false
   delete_branch_on_merge = true
   has_downloads          = false
@@ -12,6 +12,38 @@ resource "github_repository" "api-conversion" {
   has_projects           = false
   has_wiki               = false
 }
+
+
+
+resource "github_repository" "download" {
+  provider               = github.animalcrossing
+  name                   = "download"
+  description            = "Download data from acnhapi.com"
+  visibility             = "public"
+  archived               = false
+  auto_init              = true
+  delete_branch_on_merge = true
+  has_downloads          = false
+  has_issues             = false
+  has_projects           = false
+  has_wiki               = false
+}
+
+
+resource "github_repository" "convert" {
+  provider               = github.animalcrossing
+  name                   = "convert"
+  description            = "Convert downloaded data from acnhapi.com to suitable format"
+  visibility             = "public"
+  archived               = false
+  auto_init              = true
+  delete_branch_on_merge = true
+  has_downloads          = false
+  has_issues             = false
+  has_projects           = false
+  has_wiki               = false
+}
+
 
 
 resource "github_repository" "website" {
