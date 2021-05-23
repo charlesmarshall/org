fecc2dfebbe43c815ab26bbbaf57493639f814a6
Charles Marshall <cm56marshall@gmail.com>
2021-05-23 15:11:47 +0100

    move to new owner param

--- github/providers.tf
+++ github/providers.tf
@@ -4,13 +4,13 @@ provider "github" {
 }
 
 provider "github" {
-  alias        = "windmill"
-  token        = var.github_token
-  organization = "go-windmill"
+  alias = "windmill"
+  token = var.github_token
+  owner = "go-windmill"
 }
 
 provider "github" {
-  alias        = "animalcrossing"
-  token        = var.github_token
-  organization = "go-animal-crossing"
+  alias = "animalcrossing"
+  token = var.github_token
+  owner = "go-animal-crossing"
 }
