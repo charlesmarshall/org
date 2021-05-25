c20bebb87c68b6817844129c736f82d3ee4ae79a
Charles Marshall <cm56marshall@gmail.com>
2021-05-25 16:32:51 +0100

    repos

--- github/animalcrossing.tf
+++ github/animalcrossing.tf
@@ -60,12 +60,29 @@ resource "github_repository" "posts" {
 }
 
 
+resource "github_repository" "site" {
+  provider               = github.animalcrossing
+  name                   = "site"
+  description            = "Jekyll website using the generated posts and data"
+  visibility             = "public"
+  archived               = false
+  auto_init              = true
+  delete_branch_on_merge = true
+  has_downloads          = false
+  has_issues             = false
+  has_projects           = false
+  has_wiki               = false
+}
+
+
+
+
 resource "github_repository" "website" {
   provider               = github.animalcrossing
   name                   = "website"
   description            = "Templates and content to generate website"
   visibility             = "public"
-  archived               = false
+  archived               = true
   auto_init              = false
   delete_branch_on_merge = true
   has_downloads          = false
